851010ffd4dcd07041382aa535c3efbba917d583
Suresh <kingrock23041973@gmail.com>
2026-06-01 18:30:44 +0530

    Print Commented

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,7 @@ from typing import List, Any, Dict
 
 def load_all_docs(data_dir: str) -> List[Any]:
     directory_path = Path(data_dir).resolve()
-    print(f'[INFO] Directory Path is {directory_path}' )
+    # print(f'[INFO] Directory Path is {directory_path}' )
     documents = []
     pdf_files = list(directory_path.glob('**/*.pdf'))
     print(f'[INFO] Loaded {len(pdf_files)} from Directory from Dataloader.py')
